db9dbe5fe7cc3ba8b5b0ffc1fca3072730417011
guomio <guoxf1@ziroom.com>
2020-08-07 11:42:24 +0800

    init

---
+++ src/example.ts
@@ -0,0 +1,3 @@
+export function foo(params: string) {
+  return params + ' Example';
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { foo } from './example';
+
+function add(arg: number) {
+  return arg + 1;
+}
+
+console.log(foo('Hello'), add(2));
